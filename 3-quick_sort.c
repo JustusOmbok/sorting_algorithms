3544babbee02c90edde531d6a4b1fd985f120763
JustusOmbok <ombokjustus@gmail.com>
2023-07-22 00:20:16 -0700

    shell sort method

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -56,7 +56,7 @@ int lomuto_partition(int *array, int low, int high)
 	int pivot = array[high];
 	int i = low - 1, j;
 
-	for (j = low; j < high; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
